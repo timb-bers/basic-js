786fdad5d9eb54b8df590bab4554cfd9392f035c
Aliaksandr Prudnikau <timberprud@gmail.com>
2023-04-15 23:21:27 +0300

    feat: complete vigenere task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value = true) {
+    this.value = value;
+    this.alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    let index = 0;
+    let result = '';
+    message = message.toUpperCase();
+		key = key.toUpperCase();
+
+    for (let i = 0; i < message.length; i++) {
+      if (!this.alphabet.includes(message[i])) {
+        result += message[i];
+        continue;
+      }
+      result += this.alphabet[(this.alphabet.indexOf(message[i]) + this.alphabet.indexOf(key[index++ % key.length])) % this.alphabet.length];
+    }
+    return this.value ? result : result.split('').reverse().join('');
+
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    
+    let index = 0;
+    let result = '';
+    message = message.toUpperCase();
+		key = key.toUpperCase();
+
+    for (let i = 0; i < message.length; i++) {
+      if (!this.alphabet.includes(message[i])) {
+        result += message[i];    
+        continue;
+      }
+
+      let decrypt = this.alphabet.indexOf(message[i]) - this.alphabet.indexOf(key[index++ % key.length]) % this.alphabet.length;
+      if (decrypt >= 0)
+        result += this.alphabet[decrypt];  
+      else
+        result += this.alphabet[this.alphabet.length + decrypt];
+    }
+    return this.value ? result : result.split('').reverse().join('');
   }
 }
 
