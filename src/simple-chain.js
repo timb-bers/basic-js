c6c7462b46fbe59d4fefadb1d3c24dd326e773ea
Aliaksandr Prudnikau <timberprud@gmail.com>
2023-04-15 18:42:14 +0300

    feat: complete chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  arr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.arr.push(`( ${value} )`);
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (this.arr[position - 1] == undefined) {
+      this.arr = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.arr.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.arr.join("~~");
+    this.arr = [];
+    return result;
   }
 };
 
